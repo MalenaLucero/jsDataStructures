b3761d0190d0ddac02ad2e46827a91904a795db0
MalenaLucero <luceromalena@hotmail.com>
2020-10-20 12:15:56 -0300

    queue corrected

--- queue.js
+++ queue.js
@@ -31,7 +31,7 @@ function PriorityQueue(){
         } else {
             var added = false
             for(var i=0; i<collection.length; i++){
-                if(element[1] < collection[[i][1]]){
+                if(element[1] < collection[i][1]){
                     collection.splice(i,0,element)
                     added = true
                     break
